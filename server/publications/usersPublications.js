1c7c04ad326316494dad8c1bc597e12e26f628dd
dexterneo <jeremyfourna@gmail.com>
2016-02-20 12:03:55 +0100

    Update user publication

--- server/publications/usersPublications.js
+++ server/publications/usersPublications.js
@@ -2,7 +2,8 @@ Meteor.publish('allUsers', function() {
 	return Meteor.users.find({}, {
 		fields: {
 			'profile.firstName': 1,
-			'profile.lastName': 1
+			'profile.lastName': 1,
+			'profile.points': 1
 		}
 	});
 });
