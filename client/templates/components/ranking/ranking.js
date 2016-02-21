ef2c457d0e564f2d390acd84f8dcecb0ceb0a1f7
dexterneo <jeremyfourna@gmail.com>
2016-02-21 01:04:40 +0100

    Improve ranking component

--- client/templates/components/ranking/ranking.js
+++ client/templates/components/ranking/ranking.js
@@ -1,6 +1,26 @@
 class Ranking extends BlazeComponent {
 	template() {
-		return 'ranking'
+		return 'ranking';
+	}
+
+	onRendered() {
+		var userData = [];
+		for (var i = 0; i < this.data().fetch().length; i++) {
+			var list = [];
+			list.push(this.data().fetch()[i].fullName());
+			list = list.concat(this.data().fetch()[i].profile.points);
+			userData.push(list);
+		}
+		var chart = c3.generate({
+			bindto: '#rankingGraph',
+			data: {
+				type: 'line',
+				columns: userData
+			},
+		});
+		chart.load({
+			columns: userData
+		});
 	}
 }
 
