7e067fddaf1ec1c600ad8127d7b3afdaac7fc0ce
dexterneo <jeremyfourna@gmail.com>
2016-02-22 23:57:44 +0100

    Update ranking component

--- client/templates/components/ranking/ranking.js
+++ client/templates/components/ranking/ranking.js
@@ -4,11 +4,18 @@ class Ranking extends BlazeComponent {
 	}
 
 	onRendered() {
+		var freshData = Meteor.users.find({}, {
+			fields: {
+				'profile.firstName': 1,
+				'profile.lastName': 1,
+				'profile.points': 1
+			}
+		}).fetch();
 		var userData = [];
-		for (var i = 0; i < this.data().fetch().length; i++) {
+		for (var i = 0; i < freshData.length; i++) {
 			var list = [];
-			list.push(this.data().fetch()[i].fullName());
-			list = list.concat(this.data().fetch()[i].profile.points);
+			list.push(freshData[i].fullName());
+			list = list.concat(freshData[i].profile.points);
 			userData.push(list);
 		}
 		var chart = c3.generate({
@@ -21,6 +28,32 @@ class Ranking extends BlazeComponent {
 		chart.load({
 			columns: userData
 		});
+		$('tbody tr:nth-child(-n+3)').addClass('success');
+		$('tbody tr:nth-last-child(-n+3)').addClass('danger');
+	}
+
+	playerData() {
+		var freshData = Meteor.users.find({}, {
+			fields: {
+				'profile.firstName': 1,
+				'profile.lastName': 1,
+				'profile.points': 1,
+			}
+		}).fetch();
+		freshData.sort(function(a, b) {
+			if (a.average() > b.average()) {
+				return -1;
+			}
+			if (a.average() < b.average()) {
+				return 1;
+			}
+			return 0;
+		});
+		return freshData;
+	}
+
+	index() {
+		return this.currentData('@index') + 1;
 	}
 }
 
