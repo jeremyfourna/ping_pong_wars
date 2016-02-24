d83c101a9d3f45d608c8d4930407d05f49fb377f
dexterneo <jeremyfourna@gmail.com>
2016-02-25 00:48:57 +0100

    Add automatic route

--- client/lib/accounts.js
+++ client/lib/accounts.js
@@ -2,4 +2,7 @@ Accounts.onLogin(function() {
 	if ($('#navigation').hasClass('in')) {
 		$('#navigation').removeClass('in');
 	}
+	if (!Meteor.user().profile.firstName || !Meteor.user().profile.lastName) {
+		Router.go('accountWrapper');
+	}
 });
