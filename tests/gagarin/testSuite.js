c26138a20eed068148acaf0c60beb2a6f5931339
dexterneo <jeremyfourna@gmail.com>
2016-03-20 23:48:49 +0100

    Remove console.log

--- tests/gagarin/testSuite.js
+++ tests/gagarin/testSuite.js
@@ -2,7 +2,7 @@ describe('My first Gagarin test suite', function() {
 	var server = meteor();
 	it('should just work', function() {
 		return server.execute(function() {
-			console.log('I am alive!');
+			return true;
 		});
 	});
 });
