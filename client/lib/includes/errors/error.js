909bf397c833e1f353123cb062620b1099ddf408
dexterneo <jeremyfourna@gmail.com>
2016-06-12 01:05:14 +0200

    Migration to the meteor last version 10% done

--- client/lib/includes/errors/error.js
+++
@@ -1,7 +0,0 @@
-// Allow the app to remove the error message after 3 seconds
-Template.error.rendered = function() {
-	var error = this.data;
-	Meteor.setTimeout(function() {
-		Errors.remove(error._id);
-	}, 3000);
-};
--- client/lib/localCollections.js
+++
@@ -1,9 +0,0 @@
-// Local (client-only) collection
-Errors = new Mongo.Collection(null);
-
-// Function for error message
-throwError = function(message) {
-	Errors.insert({
-		message: message
-	});
-};
